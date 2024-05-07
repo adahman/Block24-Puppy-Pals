687195e502148ab189a233d52cb91512a7385418
dahmana <dhabooode@gmail.com>
2024-05-07 11:30:12 -0700

    first msg

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { puppyList } from './data.js'
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+
+// console.log("puppyList:",puppyList);
+{()=>(console.log("puppy id: ", puppy.id))}
+// console.log(featuredPup)
+
+  return (
+    <div className="App">
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+    )}
+      {
+        puppies.map((puppy) => {
+          return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
+      })
+    }
+    </div>
+  );
+}
+
+export default App
